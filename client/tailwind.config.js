37388f514c3352bbe531854b6c16140fed5fc66b
nelson <chinwubanelson@gmail.com>
2025-08-15 09:40:47 +0100

    create next app

---
+++ client/tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      container: {
+        center: true,
+        padding: "20px",
+      },
+      colors: {
+        primary: "#0025E8",
+      },
+    },
+    screens: {
+      sm: "576px",
+      md: "768px",
+      lg: "992px",
+      xl: "1200px",
+    },
+  },
+  plugins: [],
+};
